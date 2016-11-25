6da5399b5e729d1af8e85b8fbe12d70c79bc9205
PierreMassanes <pierre.massanes@gmail.com>
2016-11-25 15:52:30 +0100

    properties file and timer send duration

--- www/js/components/timerController.js
+++ www/js/components/timerController.js
@@ -6,58 +6,77 @@ angular.module('facilitation.timer', ['ionic','angular-svg-round-progress', 'ion
     var mytimeout = null; // the current timeoutID
     var timerIsSync = null;
 
-    // TODO : remove hardcoded time
-    $scope.timeForTimer = 30;
-    $scope.timer = 30;
-    $scope.started = false;
-    $scope.paused = false;
-    $scope.done = false;
-
+    // Used to join the wanted room
     $scope.synchronizeTimer = function(){
-        socket.emit('join_room', "roomTest");
+        socket.emit('join_room', $scope.roomId);
     };
 
+    // TODO : testing only, to remove
     socket.on('new_user', function(msg){
         alert(msg);
         timerIsSync = true;
     });
 
-    // actual timer method, counts down every second, stops on zero
-    $scope.onTimeout = function() {
-        if ($scope.timer === 0) {
-            $scope.$broadcast('timer-stopped', 0);
-            $timeout.cancel(mytimeout);
-            return;
-        }
-        $scope.timer--;
-        mytimeout = $timeout($scope.onTimeout, 1000);
+    // initialize the timer for time value and workshop id (for socket.io)
+    $scope.initializeTimer = function(val, workshop) {
+        $scope.timeForTimer = val;
+        $scope.timer = val;
+        $scope.started = false;
+        $scope.paused = false;
+        $scope.done = false;
+        $scope.roomId = workshop;
     };
-    // functions to control the timer
-    // starts the timer
-    $scope.startTimer = function() {
+
+    // TODO : remove hardcoded time
+    $scope.initializeTimer(60, "roomTest");
+
+    // wrap the start function for syncing
+    $scope.startSyncTimer = function() {
         if(timerIsSync) {
-            socket.emit('launch_timer', "roomTest");
-            mytimeout = $timeout($scope.onTimeout, 1000);
-            $scope.started = true;
+            var timerInfo = {"workshop":$scope.roomId,"duration":$scope.timeForTimer};
+            socket.emit('launch_timer', timerInfo);
+            startTimer();
         } else {
             alert("Sync please !");
         }
     };
 
+    // wrap the resume function for syncing
+    $scope.resumeSyncTimer = function() {
+        socket.emit('resume_timer', $scope.roomId);
+        startTimer();
+    };
+
+    // wrap the stop and reset function for syncing
+    $scope.stopSyncTimer = function(closingModal) {
+        socket.emit('stop_timer', $scope.roomId);
+        stopTimer(closingModal);
+    };
+
+    // wrap the pause function for syncing
+    $scope.pauseSyncTimer = function() {
+        socket.emit('pause_timer', $scope.roomId);
+        pauseTimer();
+    };
+
+    // functions to control the timer
+    // starts the timer
+    function startTimer(){
+        mytimeout = $timeout($scope.onTimeout, 1000);
+        $scope.started = true;
+    };
+
     // stops and resets the current timer
-    $scope.stopTimer = function(closingModal) {
-        if (closingModal != true) {
-            $scope.$broadcast('timer-stopped', $scope.timer);
-        }
+    function stopTimer(){
         $scope.timer = $scope.timeForTimer;
         $scope.started = false;
         $scope.paused = false;
         $scope.done = false;
         $timeout.cancel(mytimeout);
     };
+
     // pauses the timer
-    $scope.pauseTimer = function() {
-        $scope.$broadcast('timer-stopped', $scope.timer);
+    function pauseTimer(){
         $scope.started = false;
         $scope.paused = true;
         $timeout.cancel(mytimeout);
@@ -69,14 +88,16 @@ angular.module('facilitation.timer', ['ionic','angular-svg-round-progress', 'ion
             $scope.done = true;
         }
     });
-    // UI
-    // When you press a timer button this function is called
-    $scope.selectTimer = function(val) {
-        $scope.timeForTimer = val;
-        $scope.timer = val;
-        $scope.started = false;
-        $scope.paused = false;
-        $scope.done = false;
+
+    // actual timer method, counts down every second, stops on zero
+    $scope.onTimeout = function() {
+        if ($scope.timer === 0) {
+            $scope.$broadcast('timer-stopped', 0);
+            $timeout.cancel(mytimeout);
+            return;
+        }
+        $scope.timer--;
+        mytimeout = $timeout($scope.onTimeout, 1000);
     };
 
     // This function helps to display the time in a correct way in the center of the timer
