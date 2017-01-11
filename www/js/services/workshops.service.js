8f0e2467a27ecd688ed150c6de3fc0fbfa67a5d2
PierreMassanes <pierre.massanes@gmail.com>
2017-01-11 15:12:21 +0100

    Adding default user in connection.properties file for easier test

--- www/js/services/workshops.service.js
+++ www/js/services/workshops.service.js
@@ -17,7 +17,7 @@ app.service('WorkshopsProvider', function ($http) {
             host = response.data.serverURL;
             $http({
                 method: 'GET',
-                url: host+ '/users/unauth/Sauvage/instances'
+                url: host+ '/users/unauth/'+response.data.defaultUser+'/instances'
             }).then(function successCallback(data) {
                 var mockedWorkshop = {"_id":"roomTest","title":"Test Workshop","__v":0,"content":{"participants_profil":[],"logistics":[],"equipment":"<ul><li>TEST</li> </ul>","educational_aims":["Test"],"folklore":"Test","source":"http://availagility.co.uk/2011/07/19/running-the-ball-flow-game/","steps":[{"title":"Intro","timing":0,"duration":1,"_id":"583c255cecff901bffdb5102","description":"Test."},{"title":"Round #1","timing":1,"duration":1,"_id":"583c255cecff901bffdb5101","description":"<ul><li>Test</li></ul>"},{"title":"Round #2","timing":2,"duration":1,"_id":"583c255cecff901bffdb5100","description":"<ul><li>Test</li></ul>"},{"timing":3,"_id":"583c255cecff901bffdb50fb","description":"<center><b>Fin de l’atelier</b></center>"}]},"preparation_time":1,"synopsis":"Test","time_max":4,"time_min":4,"public_targeted":"Tous","participants_min":3,"participants_max":-1,"goals":[],"workshop_type":"Test","photo":"http://localhost:3000/img/atelier2.jpg","author":"Test"};
 
@@ -36,7 +36,7 @@ app.service('WorkshopsProvider', function ($http) {
             host = response.data.serverURL;
             $http({
                 method: 'GET',
-                url: host+'/users/unauth/Sauvage/instances/'+idWorkshop
+                url: host+'/users/unauth/'+response.data.defaultUser+'/instances/'+idWorkshop
             }).then(function successCallback(data) {
                 callback(data.data);
             }, function errorCallback(error) {
